94af4c61bab208f86d3a97ff6d801515e3b9c61a
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-12-08 18:55:47 +0600

    accordion and random color add

---
+++ 25-projects/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Accordian from './components/accordion/Accourdion'
+import RandomColor from './components/random-color/RandomColor'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+         {/* Accordion */}
+         {/* <Accordian/> */}
+
+         {/* Random color Generate */}
+         {/* <RandomColor/> */}
+      </div>
+    </>
+  )
+}
+
+export default App
